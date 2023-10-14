dad51ad48d8e3adee5bb017957c2568188a37a98
Joe Micallef <cogspa@yahoo.com>
2023-10-14 15:36:57 -0700

    random geo 1a

--- stars.py
+++ stars.py
@@ -2,10 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-def generate_star(ax, x, y, n_points, size, rotation=0):
+def generate_star(ax, x, y, n_points, size, rotation=0, color='white'):
     """Generate a star shape on given ax."""
-    star = patches.RegularPolygon((x, y), numVertices=n_points, radius=size, orientation=np.radians(rotation))
+    star = patches.RegularPolygon((x, y), numVertices=n_points, radius=size, orientation=np.radians(rotation), facecolor=color)
     ax.add_patch(star)
+
+def generate_rounded_square(ax, x, y, size, corner_radius, color='black'):
+    """Generate a rounded square."""
+    square = patches.FancyBboxPatch((x-size/2, y-size/2), size, size, boxstyle="round,pad=0.03,rounding_size="+str(corner_radius), facecolor=color)
+    ax.add_patch(square)
 
 def main():
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -13,13 +18,20 @@ def main():
     ax.set_xlim(0, 10)
     ax.set_ylim(0, 10)
     ax.axis('off')  # Turn off the axis
+    ax.set_facecolor('white')
     
-    for x in range(10):
-        for y in range(10):
+    grid_size = 10
+    square_size = 0.9
+    corner_radius = 0.15
+
+    for x in np.linspace(0.5, 9.5, grid_size):
+        for y in np.linspace(0.5, 9.5, grid_size):
+            generate_rounded_square(ax, x, y, square_size, corner_radius)
+            
             n_points = np.random.choice([4, 5, 6, 8])  # number of star points
-            size = 0.3  # for this example, size is constant, but it can be varied
+            star_size = 0.3  # size can be adjusted
             rotation = np.random.randint(0, 360)  # random rotation
-            generate_star(ax, x, y, n_points, size, rotation)
+            generate_star(ax, x, y, n_points, star_size, rotation)
     
     plt.show()
 
